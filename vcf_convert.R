3ac09b4a21e007b8702a03d071701efd3d67b273
YiruGONG <ygong2832@gmail.com>
2022-04-22 06:36:07 -0400

    add CH tag

--- vcf_convert.R
+++ vcf_convert.R
@@ -5,14 +5,14 @@ library(tidyverse)
 
 data = read.csv('2022-03-28_16-11-18_MCD289_genotypes.csv')
 
-# idx = which(colnames(data) %in% c("AA", "AC", "AF", "AFR_AF", "AMR_AF", "AN", "DP", 
-#                             "EAS_AF", "EUR_AF", "HRun", "NS", "SAS_AF", "VT", "EFF"))
-# ## How to integrate the columns
-# data[243:244,idx]
+idx = which(colnames(data) %in% c("AA", "AC", "AF", "AFR_AF", "AMR_AF", "AN", "DP", 
+                            "EAS_AF", "EUR_AF", "HRun", "NS", "SAS_AF", "VT", "EFF"))
 
 ## integrate phasing
-data = data[which(data$Covered.Case==3),]
+# data = data[which(data$Covered.Case==3),]
 
+
+###### generate mother/father info ########
 trio = data %>% 
   select(Variant.ID,Rs.Number,Sample.Name) %>% 
   mutate(value=1) %>% 
@@ -22,12 +22,65 @@ trio = data %>%
     values_from = value
   ) %>% 
   filter(GHART291b==1) %>% 
-  mutate(GHART291b=paste(GHART1542a,GHART1506,sep="|") )
+  rename("mother"=GHART1506,"father"=GHART1542a) %>% 
+  mutate(GHART291b=paste(mother,father,sep="|") )
+trio = as.data.frame(trio)
 rownames(trio) = trio$Variant.ID
 
-child = data[which(data$Sample.Name=="GHART291b"),]
-child$GHART291b = trio[child$Variant.ID,]$GHART291b
 
+###### Add CH tags ########
+## other tags tbd!!!
+child = data[which(data$Sample.Name=="GHART291b"),]
+child = cbind(child[,1:27],trio[child$Variant.ID,3:5],mut_flag=NA ,child[,28:219])
+idx = child %>% 
+  select(UpToDate.Gene.Name,GT,mother,father) %>% 
+  filter(GT=='het' | mother+father==1) %>% 
+  group_by(UpToDate.Gene.Name) %>% 
+  count() %>% 
+  filter(n>=2) %>% 
+  select(UpToDate.Gene.Name)
+idx = idx$UpToDate.Gene.Name
+child[which(child$UpToDate.Gene.Name %in% idx),'mut_flag'] = 'CH'
+
+view = select(child, GT, GHART291b)
+unique(view)
+# ## add tags
+# child[which(child$GT=="het" & child$GHART291b=="0|0"),"mut_flag"]="denovo"
+# child[which(child$GT=="het" & (child$GHART291b=="1|0" | child$GHART291b== "0|1" )),"mut_flag"]="CH"
+# child[which(child$GT=="het" & (child$GHART291b=="1|1")),"mut_flag"]="other"
+# child[which(child$GT=="hom" & child$GHART291b=="0|0"),"mut_flag"]="other"
+# child[which(child$GT=="hom" & (child$GHART291b=="1|0" | child$GHART291b== "0|1" )),"mut_flag"]="other"
+# child[which(child$GT=="hom" & (child$GHART291b=="1|1")),"mut_flag"]="homo"
+
+write.csv(child,"child_chr22.csv",row.names = F,na="")
+
+
+###### identify CH ########
+CH = child %>% 
+  filter(child$GT=="het", 
+         child$GHART291b %in% c("1|0","0|1")) %>% 
+  # separate(GHART291b,into = c('mother','father'),sep = '\\|') %>% 
+  mutate( gene = strsplit(All.Gene.Symbols,"\\|") ) %>% 
+  unnest(gene) %>% 
+  relocate(gene, .before = All.Gene.Symbols) %>% 
+  group_by(gene) %>% 
+  summarise(mother_sum = sum(as.numeric(mother)),
+            father_sum = sum(as.numeric(father)) ) %>% 
+  filter(mother_sum!=0, father_sum!=0)
+##Problem: one row only corresponding to one gene?
+
+write.csv(CH,'chr22_CH.csv',row.names = F)
+
+
+####### Compare with VarCount #######
+vc = read.table('CH_CountFile.txt',header = T)
+vc = vc[,-4] %>% filter(COUNT != 0) %>% select(-COUNT)
+setdiff(CH$gene,vc$CODING_GENE)
+intersect(CH$gene,vc$CODING_GENE)
+setdiff(vc$CODING_GENE,CH$gene)
+
+
+####### Generate VCF #######
 info = strsplit(child$Variant.ID,"-")
 info = Reduce(rbind,info)
 info = as.data.frame(info)
@@ -42,12 +95,11 @@ vcf = cbind(info,child) %>%
   select(`#CHROM`,POS,ID,REF,ALT,QUAL,FILTER,INFO,everything())
 vcf[is.na(vcf$ID),]$ID = '.'
 
-view = select(vcf, GT, GHART291b)
-unique(view)
+# view = select(vcf, GT, GHART291b)
 
 write.table(vcf,"chr22.vcf",sep = '\t',quote = F,row.names = F)
 
-vcf2 = vcf[,c(1:8,224)]
+vcf2 = cbind(vcf[,c(1:8)],GHART291b=vcf$GHART291b) 
 write.table(vcf2,"simple_chr22.vcf",sep = '\t',quote = F,row.names = F)
 
 
